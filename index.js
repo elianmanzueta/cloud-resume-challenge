95fe9032b0963f3d7defcbf1a55d2da00a567a93
Elian Manzueta <elian@Elians-MacBook-Pro.local>
2024-02-19 17:07:06 -0500

    lazycommit
    Signed-off-by: Elian Manzueta <elian@Elians-MacBook-Pro.local>

--- index.js
+++
@@ -1,9 +0,0 @@
-const counter = document.querySelector(".counter");
-
-async function updateCounter() {
-    let response = await fetch("https://qdmoubt7xmhvyfbjw3gb6t5p740ojjfl.lambda-url.us-east-2.on.aws/");
-    let data = await response.json();
-    counter.innerHTML = `This site has been visited ${data} times!`;
-}
-
-updateCounter();
