bf3542a5e70d750e7f37f658f65cfe38e7190f9c
Elian Manzueta <elian@Elians-MacBook-Pro.local>
2024-02-20 13:13:04 -0500

    Updated deploy script.
    Signed-off-by: Elian Manzueta <elian@Elians-MacBook-Pro.local>

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -5,5 +5,5 @@ terraform apply -auto-approve
 NEW_DISTRIBUTION_ID=$(terraform output distribution_id)
 NEW_S3_BUCKET=$(terraform output bucket_id)
 
-gh secret set DISTRIBUTION_ID -b"$NEW_DISTRIBUTION_ID"
+gh secret set CLOUDFRONT_DISTRIBUTION_ID -b"$NEW_DISTRIBUTION_ID"
 gh secret set S3_BUCKET -b"$NEW_S3_BUCKET"
