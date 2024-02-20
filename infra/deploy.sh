5ce2d8ad3806014e5bb106faa4017e63de1630d7
Elian Manzueta <elian@Elians-MacBook-Pro.local>
2024-02-20 11:15:03 -0500

    Removed copying to S3 from deploy.sh. Realized GitHub actions already does this on push."
    Signed-off-by: Elian Manzueta <elian@Elians-MacBook-Pro.local>

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -7,5 +7,3 @@ NEW_S3_BUCKET=$(terraform output bucket_id)
 
 gh secret set DISTRIBUTION_ID -b"$NEW_DISTRIBUTION_ID"
 gh secret set S3_BUCKET -b"$NEW_S3_BUCKET"
-
-aws s3 cp ../src/ s3://elian-cloudresumechallenge --recursive
