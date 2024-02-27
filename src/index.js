f67a1409714e78c5c77701f8582ace5ca3b78c0a
Elian Manzueta <eliandmanzueta@gmail.com>
2024-02-27 12:14:58 -0500

    tweaked index.html + formatted it further. Added comments to index.js
    Signed-off-by: Elian Manzueta <eliandmanzueta@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const counter = document.querySelector(".counter");
 
+
 async function updateCounter() {
+    /* Sends a request to the Lambda function URL */
     let response = await fetch("https://mgkjnpk3gqqcnf46dfwe3gxz4q0sweke.lambda-url.us-east-2.on.aws/");
     let data = await response.json();
     counter.innerHTML = `This site has been visited ${data} times!`;
